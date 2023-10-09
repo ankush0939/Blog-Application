c605b9e0399af138a7ea5a8936c567c85a6b2b29
Ankush Mishra <ankushmishra0939@gmail.com>
2023-10-09 22:13:47 +0530

    layout

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,8 @@
+import react from 'react'
+import styles from "./categorylist.module.css"
+
+const CategoryList = () => {
+	return <div className={styles.container}>CategoryList</div>;
+};
+
+export default CategoryList;
---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,8 @@
+import react from 'react'
+import styles from "./pagination.module.css"
+
+const Pagination = () => {
+	return <div className={styles.container}>Pagination</div>;
+};
+
+export default Pagination;
