c8977b592bd15519112b102907c903f4b0d02532
Ankush Mishra <ankushmishra0939@gmail.com>
2023-11-15 13:57:41 +0530

    api

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,11 +1,27 @@
-import react from 'react'
-import styles from "./pagination.module.css"
+'use client';
 
-const Pagination = () => {
+import react from 'react';
+import styles from './pagination.module.css';
+import { useRouter } from 'next/navigation';
+
+const Pagination = ({ page, hasPrev, hasNext }) => {
+	const router = useRouter();
 	return (
 		<div className={styles.container}>
-			<button className={styles.button}>Previous</button>
-			<button className={styles.button}>Next</button>
+			<button
+				className={styles.button}
+				disabled={!hasPrev}
+				onClick={() => router.push(`?page=${page - 1}`)}
+			>
+				Previous
+			</button>
+			<button
+				className={styles.button}
+				disabled={!hasNext}
+				onClick={() => router.push(`?page=${page + 1}`)}
+			>
+				Next
+			</button>
 		</div>
 	);
 };
