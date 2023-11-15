90687b8188bbb75fe902807d8f511a5679295a86
Ankush Mishra <ankushmishra0939@gmail.com>
2023-11-15 08:47:24 +0530

    login and authentication

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,13 +2,15 @@
 import react, { useState } from 'react';
 import styles from './authlinks.module.css';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
 
 const AuthLinks = () => {
 	const [open, setOpen] = useState(false);
-	const status = 'authenticated';
+	
+	const { status } = useSession();
 	return (
 		<>
-			{status === 'notauthenticated' ? (
+			{status === "unauthenticated" ? (
 				<Link href="/login" className={styles.link}>
 					Login
 				</Link>
@@ -17,7 +19,9 @@ const AuthLinks = () => {
 					<Link href="/login" className={styles.link}>
 						Write
 					</Link>
-					<span className={styles.link}>Logout</span>
+					<span className={styles.link} onClick={signOut}>
+						Logout
+					</span>
 				</>
 			)}
 			<div className={styles.burger} onClick={() => setOpen(!open)}>
